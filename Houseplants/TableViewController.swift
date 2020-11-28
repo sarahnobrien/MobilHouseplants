cf71147f548e6a7443ef1a29e68d7dac9e0cb425
Sarah O'Brien <snobrien@uvm.edu>
2020-11-28 03:16:31 -0500

    Got navigation controller working, reorganized my data in a way that makes sense to me: figure out the dictionary pls

--- Houseplants/TableViewController.swift
+++ Houseplants/TableViewController.swift
@@ -9,50 +9,71 @@ import UIKit
 
 //consider putting a UItable view in a UITableViewCell for the categories
 
-var plants = ["Purple Oxalis", "Golden Pothos","Neon Pothos", "Monstera Adonsonaii", "Boston Fern", "Thai Basil"]
-
-var plantLocation = ["Bedroom", "Bedroom", "Living Room", "Living Room", "Bedroom", "Kitchen", "Kitchen"]
-
-var lastWatered = ["10/30/2020", "11/2/2020", "11/4/2020", "11/1/2020", "11/2/2020", "11/3/2020"]
+class plantAreas{
+    var plantLocation: String?
+    var plants: [String?: String?]
+    //var lastWatered: String?
+    
+    init(plantLocation: String?, plants: [String?: String?]){
+        self.plantLocation = plantLocation
+        self.plants = plants
+        //self.lastWatered = lastWatered
+         }
+}
 
 var plantIndex = 0
-
+var plantLoc  = [plantAreas]()
 class TableViewController: UITableViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        plantLoc.append(plantAreas.init(plantLocation: "Kitchen", plants: ["Thai Basil" : "10/30/2020", "Italian Parsley": "10/30/2020"]))
+        
+        plantLoc.append(plantAreas.init(plantLocation: "Bedroom", plants: ["Purple Oxalis": "11/30/2020", "Golden Pothos": "11/30/2020"]))
+        
+        plantLoc.append(plantAreas.init(plantLocation: "Living Room", plants: ["Neon Pothos": "11/1/2020", "Monstera Adonsonaii": "11/1/2020"]))
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+         self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
 
-//    override func numberOfSections(in tableView: UITableView) -> Int {
-//        // #warning Incomplete implementation, return the number of sections
-//        return 0
-//    }
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return plantLoc.count
+    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return plants.count
+        return plantLoc[section].plants.count ?? 0 //0 is default if nothing in the section
+        //return plants.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        
-        cell.textLabel?.text = plants[indexPath.row]
-
+        cell.textLabel?.text = plantLoc[indexPath.section].plants?[indexPath.row]
+        //cell.textLabel?.text = plants[indexPath.row]
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return plantLoc[section].plantLocation
     }
     
    
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         plantIndex = indexPath.row
-        performSegue(withIdentifier: "segue", sender: self)
+        performSegue(withIdentifier: "viewControllerSegue", sender: self)
+        
+        
+//        navigationController?.pushViewController(ViewController(), animated: true)
+        
+//        performSegue(withIdentifier: "segue", sender: self)
     }
     
 
@@ -60,16 +81,31 @@ class TableViewController: UITableViewController {
         return .delete
     }
     
+//    func showAlert() {
+//        let alert = UIAlertController(title: "Delete", message: "Are you sure you would like to delete this entry?", preferredStyle: .alert)
+//        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: {action in
+//            print("cancelled")
+//        }))
+//        alert.addAction(UIAlertAction(title: "Delete", style: .destructive, handler: {action in
+//            print("deleted")
+//        }))
+//        present(alert, animated: true)
+//    }
    // Override to support editing the table view.
-   override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-       if editingStyle == .delete {
-            //Delete the row from the data source
-        plants.remove(at: indexPath.row)
-           tableView.deleteRows(at: [indexPath], with: .fade)
-       } else if editingStyle == .insert {
-            //Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-       }
-   }
+   
+    // MARK: - DELETE PORTION COME BACK ASFJHBG.
+    
+//    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+//        //showAlert()
+//
+//        if editingStyle == .delete {
+//            //Delete the row from the data source
+//            plants.remove(at: indexPath.row)
+//           tableView.deleteRows(at: [indexPath], with: .fade)
+//       } else if editingStyle == .insert {
+//            //Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+//       }
+//   }
     
     
 
--- Houseplants/ViewController.swift
+++ Houseplants/ViewController.swift
@@ -8,18 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+//    @IBOutlet weak var plantName: UILabel?
+//    @IBOutlet weak var plantLocations: UILabel?
+//   // @IBOutlet weak var waterDate: UILabel!
+//
+    
     @IBOutlet weak var plantName: UILabel!
-    @IBOutlet weak var plantLocations: UILabel!
-    @IBOutlet weak var waterDate: UILabel!
+    @IBOutlet weak var plantLocation: UILabel!
+    @IBOutlet weak var lastWatered: UILabel!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        plantName.text = plants[plantIndex]
-        plantLocations.text = plantLocation[plantIndex]
-        waterDate.text = lastWatered[plantIndex]
+        let locations = plantLoc[plantIndex].plantLocation
+        let plants = plantLoc[plantIndex].plants
+        let watered = plantLoc[plantIndex].lastWatered
+        
+        plantName?.text = plants?[plantIndex]
+        plantLocation?.text = locations
+        lastWatered?.text = watered?[plantIndex]
+        
+        //plantName.text = plantLoc.plants[plantIndex]
+        
+        //plantLocations.text = plantLoc.plantLocation?
+        
+        //plantName.text = plants[plantIndex]
+        //plantLocations.text = plantLocation[plantIndex]
+        //waterDate.text = lastWatered[plantIndex]
+        
+       
         
     }
+    
 
 
 }
